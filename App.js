d9369a629e76f8b2aa6f5bca46daf30714364f26
phamtanphat <phatdroid94@gmail.com>
2021-05-18 18:08:29 +0700

    ui filter

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import {SafeAreaView} from 'react-native';
 import Main from './src/screens/Main';
 import Box from './src/components/Box';
 import Form from './src/components/Form';
+import Filter from './src/components/Filter';
 
 export default class App extends Component {
   render() {
     return (
       <SafeAreaView style={{flex: 1}}>
-        <Main />
+        {/* <Main /> */}
         {/* <Form/> */}
+        <Filter />
       </SafeAreaView>
     );
   }
---
+++ src/components/Filter.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import {StyleSheet, View} from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
+
+export default class Filter extends Component {
+  render() {
+    return (
+      <View style={styles.containerPickerStyle}>
+        <RNPickerSelect
+          items={[
+            {label: 'Show All', value: 'Show_All'},
+            {label: 'Show Forgot', value: 'Show_Forgot'},
+            {label: 'Show Memorized', value: 'Show_Memorized'},
+          ]}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  containerPickerStyle: {
+    borderWidth: 1,
+    borderRadius: 1,
+    borderColor: 'black',
+    padding: 20,
+    marginBottom: 5,
+  },
+  pickerStyle: {
+    padding: 50,
+  },
+});
