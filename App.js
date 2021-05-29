855ba294acc168937d430d212b25a32957d3cdb0
phamtanphat <phatdroid94@gmail.com>
2021-05-29 19:15:16 +0700

    tach tat ca redux ra ngoai

--- App.js
+++ App.js
@@ -6,57 +6,8 @@ import Box from './src/components/Box';
 import Form from './src/components/Form';
 import Filter from './src/components/Filter';
 import Word from './src/components/Word';
-import {createStore, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
-
-const defaultWords = [
-  {id: 1, en: 'One', vn: 'Một', isMemorized: false},
-  {id: 2, en: 'Two', vn: 'Hai', isMemorized: false},
-  {id: 3, en: 'Three', vn: 'Ba', isMemorized: true},
-  {id: 4, en: 'Four', vn: 'Bốn', isMemorized: true},
-  {id: 5, en: 'Five', vn: 'Năm', isMemorized: false},
-];
-
-function wordReducer(state = defaultWords, action) {
-  if (action.type === 'ADD_WORD') {
-    const newWords = state.map((word) => {
-      return {...word};
-    });
-    newWords.push(action.newWord);
-    return newWords;
-  }
-  if (action.type === 'TOGGLE_WORD') {
-    const newWords = state.map((item) => {
-      if (item.id === action.word.id) {
-        return {...item, isMemorized: !item.isMemorized};
-      }
-      return item;
-    });
-    return newWords;
-  }
-  if (action.type === 'REMOVE_WORD') {
-    const newWords = state.words.filter((item) => {
-      if (item.id === action.word.id) {
-        return false;
-      }
-      return true;
-    });
-    return newWords;
-  }
-  return state;
-}
-function filterModeReducer(state = null, action) {
-  if (action.type === 'SET_FILTER_MODE') {
-    return action.filterMode;
-  }
-  return state;
-}
-const reducer = combineReducers({
-  words: wordReducer,
-  filterMode: filterModeReducer,
-});
-
-const store = createStore(reducer);
+import store from './src/redux/store';
 
 export default class App extends Component {
   render() {
